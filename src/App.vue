4e623514803f3d408fde97fa3efd0a0e9a49e633
kwang_hoon <kwanghoon222@gmail.com>
2019-11-22 01:07:37 +0900

    11.5 vuejs study #2

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <tempcom></tempcom>
+  </div>
+</template>
+
+<script>
+import tempcom from "./components/tempcom";
+
+export default {
+  name: "app",
+
+  components: {
+    tempcom
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./style/style.scss";
+</style>
---
+++ src/components/tempcom.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="tempcom">
+    <h1>여기는 component1 입니다.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tempcom",
+  data() {
+    return {};
+  }
+};
+</script>
